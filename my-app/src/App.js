25686b7429be1056e2be6c708369c69d58f07879
phong <phongdbde160574@fpt.edu.vn>
2023-06-21 07:26:49 +0700

    React Router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,20 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {Component}  from 'react';
-import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
-
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-   
-  }
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+     
+    <BrowserRouter>
+    <div className="App">
+      <Main />
+    </div>
+  </BrowserRouter>
+  
+
     );
   }
 }
